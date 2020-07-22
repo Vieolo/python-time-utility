eeac37bd56907b70f07f16f5eb3a4a3bb4c3671e
Ramtin <ramtin.abadi@gmail.com>
2020-07-22 15:42:57 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="time-utility",  # Replace with your own username
+    version="0.1.0",
+    author="Ramtin Abadi",
+    author_email="Ramtin.Abadi@gmail.com",
+    description="Time utility functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ramtinabadi/python-time-utility.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ time-utility/time_utility.py
@@ -0,0 +1,219 @@
+from datetime import datetime, timedelta, date
+from calendar import monthrange
+from typing import Tuple
+
+import pytz
+
+
+class TimeUtility:
+    """Date and Time utility functions"""
+
+    # Period Constants
+    DAILY = "daily"
+    MONTHLY = "monthly"
+    ANNUAL = "annual"
+
+    # Time Unit Constants
+    MICROSECOND = "microsecond"
+    SECOND = "second"
+    MINUTE = "minute"
+    HOUR = "hour"
+    DAY = "day"
+
+    @staticmethod
+    def now(timezone=pytz.utc) -> datetime:
+        """
+        Returns the current date and time with the given time zone
+
+        :param timezone: The desired timezone, defaults to UTC
+        """
+        return datetime.now(tz=timezone)
+
+    @staticmethod
+    def today(timezone=pytz.utc) -> date:
+        """
+        Returns the current date with the given timezone
+
+        :param timezone: The desired timezone, defaults to UTC
+        """
+        return datetime.now(tz=timezone).date()
+
+    @staticmethod
+    def get_date_start(timezone=pytz.utc, year: int = None, month: int = None, day: int = None) -> datetime:
+        """
+        Returns the date and time of the beginning of the day with the given timezone
+
+        :param timezone: The desired timezone, defaults to UTC
+        :param year: The target year, ignore or pass None to use the current year
+        :param month: The target month, ignore or pass None to use the current month
+        :param day: The target day, ignore or pass None to use the current day
+        """
+        return datetime.now(tz=timezone).replace(
+            year=year if year is not None else datetime.now(tz=timezone).year,
+            month=month if month is not None else datetime.now(tz=timezone).month,
+            day=day if day is not None else datetime.now(tz=timezone).day,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0
+        )
+
+    @staticmethod
+    def get_date_end(timezone=pytz.utc, year: int = None, month: int = None, day: int = None) -> datetime:
+        """
+        Returns the date and time of the ending of the day with the given timezone
+
+        :param timezone: The desired timezone, defaults to UTC
+        :param year: The target year, ignore or pass None to use the current year
+        :param month: The target month, ignore or pass None to use the current month
+        :param day: The target day, ignore or pass None to use the current day
+        """
+        return datetime.now(tz=timezone).replace(
+            year=year if year is not None else datetime.now(tz=timezone).year,
+            month=month if month is not None else datetime.now(tz=timezone).month,
+            day=day if day is not None else datetime.now(tz=timezone).day,
+            hour=23,
+            minute=59,
+            second=59,
+            microsecond=999999
+        )
+
+    @staticmethod
+    def get_month_start(timezone=pytz.utc, year: int = None, month: int = None) -> datetime:
+        """
+        Returns the date and time of the beginning of the month with the given timezone
+
+        :param timezone: The desired timezone, defaults to UTC
+        :param year: The target year, ignore or pass None to use the current year
+        :param month: The target month, ignore or pass None to use the current month
+        """
+        return datetime.now(tz=timezone).replace(
+            year=year if year is not None else datetime.now(tz=timezone).year,
+            month=month if month is not None else datetime.now(tz=timezone).month,
+            day=1,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0
+        )
+
+    @staticmethod
+    def get_month_end(timezone=pytz.utc, year: int = None, month: int = None) -> datetime:
+        """
+        Returns the date and time of the ending of the month with the given timezone
+
+        :param timezone: The desired timezone, defaults to UTC
+        :param year: The target year, ignore or pass None to use the current year
+        :param month: The target month, ignore or pass None to use the current month
+        """
+        return datetime.now(tz=timezone).replace(
+            year=year if year is not None else datetime.now(tz=timezone).year,
+            month=month if month is not None else datetime.now(tz=timezone).month,
+            day=monthrange(year, month)[1],
+            hour=23,
+            minute=59,
+            second=59,
+            microsecond=999999
+        )
+
+    @staticmethod
+    def get_year_start(timezone=pytz.utc, year: int = None):
+        """
+        Returns the date and time of the beginning of the year with the given timezone
+
+        :param timezone: The desired timezone, defaults to UTC
+        :param year: The target year, ignore or pass None to use the current year
+        """
+        return datetime.now(tz=timezone).replace(
+            year=year if year is not None else datetime.now(tz=timezone).year,
+            month=1,
+            day=1,
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0
+        )
+
+    @staticmethod
+    def get_year_end(timezone=pytz.utc, year: int = None):
+        """
+        Returns the date and time of the ending of the year with the given timezone
+
+        :param timezone: The desired timezone, defaults to UTC
+        :param year: The target year, ignore or pass None to use the current year
+        """
+        return datetime.now(tz=timezone).replace(
+            year=year if year is not None else datetime.now(tz=timezone).year,
+            month=12,
+            day=31,
+            hour=23,
+            minute=59,
+            second=59,
+            microsecond=999999
+        )
+
+    @staticmethod
+    def adjust_offset(original_datetime: datetime, offset: int, local_to_utc: bool) -> datetime:
+        """
+        Returns a new datetime object by adjusting the original datetime according to the given offset (in minutes). Note that Javascript offset should be multiplied by -1
+
+        :param original_datetime: The original and unchanged datetime object
+        :param offset: The desired offset in minutes (Note that the Javascript offset obtained via `new Date().getTimezoneOffset()` should be multiplied by -1)
+        :param local_to_utc: Set to True if the original datetime is in local time and set to False if the original datetime is in UTC timezone
+        """
+        if local_to_utc:
+            new_time = original_datetime - timedelta(minutes=offset)
+        else:
+            new_time = original_datetime + timedelta(minutes=offset)
+        return new_time
+
+    @staticmethod
+    def get_period(year: int, month: int, day: int, period: str, offset: int = 0, local_to_utc: bool = False) -> Tuple[datetime, datetime]:
+        """
+        Returns the beginning and the ending datetime of a period
+
+        :param year: The target year
+        :param month: The target month
+        :param day: The target day
+        :param period: The desired period time-span. Please use the period constants of TimeUtility such as `TimeUtility.DAILY'
+        :param offset: The optional timezone offset in minutes. (Note that the Javascript offset obtained via `new Date().getTimezoneOffset()` should be multiplied by -1)
+        :param local_to_utc: Set to True if the original datetime is in local time and set to False if the original datetime is in UTC timezone
+        """
+        if period == TimeUtility.DAILY:
+            start = TimeUtility.get_date_start(year, month, day)
+            end = TimeUtility.get_date_end(year, month, day)
+        elif period == TimeUtility.MONTHLY:
+            start = TimeUtility.get_month_start(year, month)
+            end = TimeUtility.get_month_end(year, month)
+        elif period == TimeUtility.ANNUAL:
+            start = TimeUtility.get_year_start(year)
+            end = TimeUtility.get_year_end(year)
+        else:
+            raise ValueError()
+
+        if offset == 0:
+            return start, end
+
+        return TimeUtility.adjust_offset(start, offset, local_to_utc), TimeUtility.adjust_offset(end, offset, local_to_utc)
+
+    @staticmethod
+    def difference(large_time: datetime, small_time: datetime, time_span: str = 'second') -> int:
+        """
+        The difference between two datetime objects in microseconds, seconds, minutes, hours, or days
+
+        :param large_time: The larger datetime object
+        :param small_time: The smaller datetime object
+        :param time_span: The time-span of difference. Please use the time unit constants of TimeUtility such as `TimeUtility.HOUR`
+        """
+        if time_span == TimeUtility.SECOND:
+            return abs((large_time - small_time).seconds)
+        elif time_span == TimeUtility.MICROSECOND:
+            return abs((large_time - small_time).microseconds)
+        elif time_span == TimeUtility.DAY:
+            return abs((large_time - small_time).days)
+        elif time_span == TimeUtility.MINUTE:
+            return int(abs((large_time - small_time).seconds)/60)
+        elif time_span == TimeUtility.HOUR:
+            return int(abs((large_time - small_time).seconds)/3600)
+        else:
+            raise ValueError()
