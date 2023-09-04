892b6d68ef859e9c21fce946d392f64cca9605d4
Ramtin <Ramtin.abadi@gmail.com>
2023-09-05 03:51:06 +0530

    Version Bump to 0.2.1

---
+++ changelog.md
@@ -0,0 +1,4 @@
+# Change Log
+
+## v0.2.1 (2023-09-05)
+- Fixed the types
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="time_utility",  # Replace with your own username
-    version="0.2.0",
+    version="0.2.1",
     author="Vieolo OÜ",
     author_email="info@vieolo.com",
     description="Time utility functions",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.10',
 )
